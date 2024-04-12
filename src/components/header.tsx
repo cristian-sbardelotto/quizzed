451a3217d639a4b8f2be1ae1928548e909ecb6e2
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-12 15:03:40 -0300

    feat: add link to home on logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,14 @@
 import { GithubIcon } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
     <header className='py-8 px-4 flex items-center justify-between'>
-      <h1 className='text-2xl text-gray-200 font-bold md:text-3xl'>
-        ⚡ Quiz<span className='text-gray-500'>zed</span>
-      </h1>
+      <Link to='/'>
+        <h1 className='text-2xl text-gray-200 font-bold md:text-3xl'>
+          ⚡ Quiz<span className='text-gray-500'>zed</span>
+        </h1>
+      </Link>
 
       <a
         className='rounded-2xl text-gray-200 p-2 hover:bg-slate-50/10 transition-colors'
