8b2c9622cc656dfff82dfec2b16c0a273c9b2522
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-15 14:12:25 -0300

    feat: add card component

---
+++ src/components/card.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react';
+
+import { twMerge } from 'tailwind-merge';
+
+type CardProps = {
+  icon: string;
+  children: ReactNode;
+  description: string;
+  variant?: 'default' | 'correct' | 'wrong';
+};
+
+export function Card({
+  children,
+  description,
+  icon,
+  variant = 'default',
+}: CardProps) {
+  let variantClasses = '';
+
+  switch (variant) {
+    case 'correct':
+      variantClasses =
+        'border-emerald-500 shadow-correct-option bg-emerald-500/5';
+      break;
+    case 'wrong':
+      variantClasses = 'border-red-600 shadow-wrong-option bg-red-600/5';
+      break;
+    default:
+      variantClasses = 'border-gray-600 shadow-option';
+  }
+
+  return (
+    <div
+      className={twMerge(
+        'flex flex-col items-center py-5 px-6 gap-3 rounded-2xl w-fit border-2 text-gray-100',
+        variantClasses
+      )}
+    >
+      <span className='text-2xl'>{icon}</span>
+
+      <p className='font-bold text-xl'>{children}</p>
+
+      <span className='text-lg'>{description}</span>
+    </div>
+  );
+}
