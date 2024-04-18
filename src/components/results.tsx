116b73db39303f5253d9ed6b63480d232c4dff52
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-18 14:47:04 -0300

    feat: add specific results messages

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -10,11 +10,85 @@ type ResultsProps = {
 export function Results({ correctAnswers, questionsAmount }: ResultsProps) {
   const wrongAnswers = questionsAmount - correctAnswers;
 
+  function calculatePrecision({
+    correctAnswers,
+    questionsAmount,
+  }: ResultsProps) {
+    const precisionAverage = Math.round(
+      (correctAnswers / questionsAmount) * 100
+    );
+
+    if (precisionAverage === 0) {
+      return 'terrible';
+    }
+    if (precisionAverage === 100) {
+      return 'perfect';
+    }
+    if (precisionAverage <= 25) {
+      return 'bad';
+    }
+    if (precisionAverage > 25 && precisionAverage <= 75) {
+      return 'good';
+    }
+
+    return 'great';
+  }
+
+  const resultsMessage = calculatePrecision({
+    correctAnswers,
+    questionsAmount,
+  });
+
+  const resultsMessagesDictionary = {
+    terrible: (
+      <>
+        <span className='text-gray-100'>Don't worry</span>, everyone starts
+        somewhere. Let's work on it!.
+      </>
+    ),
+    bad: (
+      <>
+        You did a <span className='text-gray-100'>good job</span>! Time to
+        review and strengthen your knowledge!
+      </>
+    ),
+    good: (
+      <>
+        <span className='text-gray-100'>Keep up</span> the good work and focus
+        on areas where you can improve.
+      </>
+    ),
+    great: (
+      <>
+        You're <span className='text-gray-100'>among the best!</span> You're
+        doing exceptionally well!
+      </>
+    ),
+    perfect: (
+      <>
+        You did it perfectly! You're a{' '}
+        <span className='text-gray-100'>quiz wizard</span>!
+      </>
+    ),
+  };
+
+  const resultsTitlesDictionary = {
+    terrible: 'What a shame! 😥',
+    bad: 'Good job! 👍🏻',
+    good: 'Cool! 💪🏻',
+    great: 'Fantastic! 🚀',
+    perfect: 'Congrats! 🎉',
+  };
+
   return (
-    <div>
-      <h2 className='text-center text-2xl md:text-4xl text-gray-100 font-extrabold'>
-        🎉 Congrats!
-      </h2>
+    <>
+      <div className='flex flex-col gap-6 px-5 md:items-center'>
+        <h2 className='text-2xl md:text-4xl text-gray-100 font-extrabold'>
+          {resultsTitlesDictionary[resultsMessage]}
+        </h2>
+
+        <p>{resultsMessagesDictionary[resultsMessage]}</p>
+      </div>
 
       <section className='flex flex-row flex-wrap justify-center gap-4 px-4 md:gap-16 mt-10'>
         <Card
@@ -61,6 +135,6 @@ export function Results({ correctAnswers, questionsAmount }: ResultsProps) {
           </button>
         </Link>
       </div>
-    </div>
+    </>
   );
 }
