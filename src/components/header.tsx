da6b069d5ff3b2f715ca20027f0ddc8cae7bbd4e
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-21 00:39:04 -0300

    fix: fix bug of pointer events on header and footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
-    <header className='py-8 px-4 flex items-center justify-between animate-appear pointer-events-none'>
+    <header className='py-8 px-4 flex items-center justify-between animate-appear'>
       <Link to='/'>
         <h1 className='text-2xl text-gray-200 font-bold md:text-3xl hover:brightness-[.8] transition-[filter]'>
           ⚡ Quiz<span className='text-gray-500'>zed</span>!
